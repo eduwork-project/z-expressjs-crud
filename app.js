295564646dfab80a9d10da18297b7518ce94b4ba
Dhoni <ramadhoni@keyreply.com>
2022-09-06 13:49:57 +0700

    prepared endpoint crud

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+})
+
+// endpoint for data member
+app.get("/dataMember", (req, res) => {
+  
+})
+app.post("/dataMember", (req, res) => {
+  
+})
+app.get("/dataMember/:id", (req, res) => {
+  
+})
+app.patch("/dataMember/:id", (req, res) => {
+  
+})
+app.delete("/dataMember/:id", (req, res) => {
+  
+})
+
 app.listen(3000, () => {
   console.log("running on port 3000...");
 })
